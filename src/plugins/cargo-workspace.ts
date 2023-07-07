701346110ab66f346ab11537f7c55482aadaade6
rohanku <rohankumar@berkeley.edu>
2023-07-07 10:51:05 -0700

    feat(dev-deps): don't bump dev dependencies

--- src/plugins/cargo-workspace.ts
+++ src/plugins/cargo-workspace.ts
@@ -423,6 +423,9 @@ function getChangelogDepsNotes(
     updates: DepUpdates
   ) => {
     for (const depType of depTypes) {
+      // Skip updating dev dependencies as they don't matter for publishing purposes.
+      if (depType === 'dev-dependencies') { continue; }
+
       const depUpdates = [];
       const pkgDepTypes = updatedScope[depType];
 
